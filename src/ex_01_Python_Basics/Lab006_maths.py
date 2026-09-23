c6a7c2669b60c8828ac119b3bee9467122f83cac
Shruti Saxena <shrutisaxena9703@gmail.com>
2026-09-23 22:54:56 +0200

    "add all these updated files"

--- src/ex_01_Python_Basics/Lab006_maths.py
+++ src/ex_01_Python_Basics/Lab006_maths.py
@@ -7,3 +7,20 @@ print(c)
 #bodmass problem
 d=(a+b)*c+a-b
 print(d)
+
+
+#multiple variables
+
+age=30
+name="shruti"
+number=17.12
+print(age,name,number,sep= "|")
+
+#maths functions are in-built like max
+#max number between 3,4 since max function is function can take infinite args
+result=max(3,4)
+print(result)
+result_min=min(1,0,-1,1000,56,-4,-7)
+print(result_min)
+
+
